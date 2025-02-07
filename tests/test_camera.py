61d6b35b76f9517650d2c3b34ade5cf136a6e3dd
Neil Lamoureux <neil.lamoureux@gmail.com>
2025-02-06 21:01:08 -0700

    Futz with camera tests

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -1,5 +1,5 @@
 from copy import copy
-from math import acos
+from math import acos, cos
 
 from pytest import approx, mark
 import numpy as np
@@ -130,27 +130,41 @@ def test_projection_matrix():
 
 @mark.skip(reason="Not sure how to test")
 def test_projection_matrix2():
+    # Note: OpenGL is right-handed, so the z-axis is pointing out of the screen back to the viewer
     cam = Camera()
     cam.aspect_ratio = 2.0
-    cam.fovy = 90.0
+    cam.fovy = 45.0
     cam.near = 1.0
     cam.far = 100.0
     projection_matrix = cam.projection_matrix
     assert projection_matrix.shape == (4, 4)
     # near should map to [0, 0, 1] in clip space, or in 4 dim [0, 0, z, z], z arbitrary
     near_product = projection_matrix.dot(np.array([0.0, 0.0, -cam.near, 1.0]))
-    # assert near_product == approx(np.array([0.0, 0.0, 1.0, -1.0])) # just to see the values
     assert near_product[0:2] == approx(np.array([0.0, 0.0]))
-    assert near_product[2] == approx(-near_product[3])
+    assert near_product[2] == approx(near_product[3])
     # far should map to [0, 0, -1] in clip space, or in 4 dim [0, 0, z, -z], z arbitrary
     far_product = projection_matrix.dot(np.array([0.0, 0.0, -cam.far, 1.0]))
-    # assert far_product == approx(np.array([0.0, 0.0, 1.0, -1.0])) # just to see the values
     assert far_product[0:2] == approx(np.array([0.0, 0.0]))
-    assert far_product[2] == approx(far_product[3])
+    # assert far_product[2] / far_product[3] == approx(-1.0)
+    assert far_product[2] == approx(-far_product[3])
 
 
-def test_approx():
-    assert np.array([1.0, 2.0]) == approx(np.array([1.0, 2.0]))
+# test fovy
+@mark.skip(reason="Not sure how to test")
+def test_projection_matrix3():
+    # Note: OpenGL is right-handed, so the z-axis is pointing out of the screen back to the viewer
+    cam = Camera()
+    cam.aspect_ratio = 2.0
+    cam.fovy = 45.0
+    cam.near = 1.0
+    cam.far = 100.0
+    projection_matrix = cam.projection_matrix
+    assert projection_matrix.shape == (4, 4)
+    #  aspect_ratio * tan(fovy/2),tan(fov/2), near should map to [1.0, 1.0, 1] in clip space, or in 4 dim [1, z, z, z], z arbitrary
+    y = cam.near * np.tan(np.radians(cam.fovy) / 2)
+    x = y * cam.aspect_ratio
+    near_product = projection_matrix.dot(np.array([x, y, -cam.near, 1.0]))
+    assert near_product / near_product[3] == approx(np.array([1.0, 1.0, 1.0, 1.0]))
 
 
 def test_orbit_look_at_unchanged():
@@ -169,29 +183,12 @@ def test_orbit_direction_rotated():
     cam.look_at = np.array([-3.5, -2.5, -1.5])
     initial_direction = copy(cam.direction)
     cam.orbit(np.pi / 1.2, np.pi / 3.1)
-    rotation = acos(
-        np.dot(initial_direction, cam.direction)
-        / (np.linalg.norm(initial_direction) * np.linalg.norm(cam.direction))
+    cos_rotation = np.dot(initial_direction, cam.direction) / (
+        np.linalg.norm(initial_direction) * np.linalg.norm(cam.direction)
     )
-    assert rotation == approx(np.pi / 3.1)
+    assert cos_rotation == approx(np.cos(np.pi / 3.1))
 
 
-@mark.skip(reason="Is this the right test")
-def test_orbit_up_rotated():
-    cam = Camera()
-    cam.up = np.array([1.0, 1.0, 0.0])
-    cam.position = np.array([1.0, 2.0, 3.0])
-    cam.look_at = np.array([-3.5, -2.5, -1.5])
-    initial_perp_up = copy(cam.perpendicular_up)
-    cam.orbit(np.pi / 1.2, np.pi / 3.1)
-    rotation = acos(
-        np.dot(initial_perp_up, cam.up)
-        / (np.linalg.norm(initial_perp_up) * np.linalg.norm(cam.up))
-    )
-    assert rotation == approx(np.pi / 1.2)
-
-
-# to do for orbit
 # test position - look_at has unchanged length
 def test_orbit_position_look_at_length_unchanged():
     cam = Camera()
@@ -202,20 +199,36 @@ def test_orbit_position_look_at_length_unchanged():
     assert np.linalg.norm(cam.look_at - cam.position) == approx(initial_length)
 
 
-# test rotation axis and the angle of rotation between initial perp_up and final perp_up is the angle from up rotation param
-@mark.skip(reason="Is this the right test")
-def test_orbit_rotation_axis():
+# test rotation axis perpendicular up = angle from up + pi / 2
+def test_orbit_up_rotation_axis_angle_90():
     cam = Camera()
     cam.up = np.array([1.0, 1.0, 0.0])
     cam.position = np.array([1.0, 2.0, 3.0])
     cam.look_at = np.array([-3.5, -2.5, -1.5])
-    # initial_up = copy(cam.up)
-    initial_perp_up = copy(cam.perpendicular_up)
     initial_direction = copy(cam.direction)
-    cam.orbit(np.pi / 1.2, np.pi / 3.1)
-    rotation_axis = np.cross(initial_direction, cam.direction)
-    rotation_axis_angle = acos(
-        np.dot(initial_perp_up, rotation_axis)
-        / (np.linalg.norm(initial_perp_up) * np.linalg.norm(rotation_axis))
+    initial_perp_up = copy(cam.perpendicular_up)
+    # cam.orbit(np.pi / 1.2, np.pi / 3.1)
+    cam.orbit(np.pi / 2, np.pi / 3.1)
+    rotation_axis = np.cross(cam.direction, initial_direction)
+    cos_rotation_axis_angle = np.dot(initial_perp_up, rotation_axis) / (
+        np.linalg.norm(initial_perp_up) * np.linalg.norm(rotation_axis)
     )
-    assert rotation_axis_angle == approx(np.pi / 1.2)
+    assert cos_rotation_axis_angle == approx(np.cos(np.pi / 2 - np.pi / 2))
+
+
+# test rotation axis perpendicular up = angle from up + pi / 2
+def test_orbit_up_rotation_axis_angle():
+    cam = Camera()
+    cam.up = np.array([1.0, 1.0, 0.0])
+    cam.position = np.array([1.0, 2.0, 3.0])
+    cam.look_at = np.array([-3.5, -2.5, -1.5])
+    initial_direction = copy(cam.direction)
+    initial_perp_up = copy(cam.perpendicular_up)
+    cam.orbit(np.pi / 1.2, np.pi / 3.1)
+    rotation_axis = np.cross(cam.direction, initial_direction)
+    print(rotation_axis)
+    print(initial_perp_up)
+    cos_rotation_axis_angle = np.dot(initial_perp_up, rotation_axis) / (
+        np.linalg.norm(initial_perp_up) * np.linalg.norm(rotation_axis)
+    )
+    assert cos_rotation_axis_angle == approx(np.cos(np.pi / 1.2 - np.pi / 2))
