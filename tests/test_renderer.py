546a965201adcbf382dfe13ee99b78d9ad387c54
Neil Lamoureux <neil.lamoureux@gmail.com>
2025-03-09 22:22:07 -0600

    Add renderer tests

---
+++ tests/test_renderer.py
@@ -0,0 +1,49 @@
+from unittest.mock import MagicMock, Mock, patch
+
+from pyrr import Matrix44
+import numpy as np
+
+from meshsee.camera import Camera
+from meshsee.renderer import Renderer
+
+
+def test_init():
+    context = MagicMock()
+    camera = Camera()
+    aspect_ratio = 1.6
+    Renderer.frame = Mock()
+    Renderer.load_mesh = Mock()
+    renderer = Renderer(context, camera, aspect_ratio)
+    assert renderer.aspect_ratio == aspect_ratio
+    context.clear.assert_called_with(*Renderer.BACKGROUND_COLOR)
+    context.program.assert_called()
+    renderer.frame.assert_called_once()
+    renderer.load_mesh.assert_called_with(renderer._default_mesh)
+
+
+def test_aspect_ratio():
+    context = MagicMock()
+    m_proj = Mock()
+    m_camera = Mock()
+    m_model_write = Mock()
+    color_write = Mock()
+    shader_vars = {
+        "m_proj": m_proj,
+        "m_camera": m_camera,
+        "m_model": m_model_write,
+        "color": color_write,
+    }
+    context.program = Mock(return_value=shader_vars)
+    camera = Camera()
+    aspect_ratio = 0.5
+    renderer = Renderer(context, camera, aspect_ratio)
+    assert renderer.aspect_ratio == aspect_ratio
+    new_aspect_ratio = 1.6
+    renderer.aspect_ratio = new_aspect_ratio
+    assert renderer.aspect_ratio == new_aspect_ratio
+    assert camera.aspect_ratio == new_aspect_ratio
+    assert np.array_equal(m_proj.write.call_args[0][0], camera.projection_matrix)
+    # assert np.array_equal(m_camera.write.call_args[0][0], camera.view_matrix)
+    # m_model_write.assert_called_with(Matrix44.identity(dtype="f4"))
+    # color_write.assert_called_with(Renderer.BACKGROUND_COLOR)
+    # assert shader_vars["m_proj"] == camera.projection_matrix
