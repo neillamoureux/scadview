532ef147c612c7678470117300ba3dc01d544e6c
Neil Lamoureux <neil.lamoureux@gmail.com>
2025-06-02 17:16:30 -0600

    clean up code

--- src/meshsee/font.py
+++ src/meshsee/font.py
@@ -1,6 +1,5 @@
 import os
 from functools import cache
-from time import time
 
 import numpy as np
 import trimesh
@@ -22,8 +21,7 @@ SIZE_MULTIPLIER = 1.374  # Used to convert pt size to mesh units
 
 def create_mesh():
     t = "".join(chr(i) for i in range(32, 127))
-    # t = "A0"
-    font = "Palatino Linotype:style=Italic"
+    font = "Papyrus:style=Condensed"
     return text(t, font=font, size=100).apply_scale((1.0, 1.0, 10.0))
 
 
@@ -34,19 +32,9 @@ def list_system_fonts() -> dict[str:str]:
     (only TrueType/OpenType fonts).
     """
     # findSystemFonts returns absolute paths to .ttf/.otf files
-    start = time()
     font_paths = font_manager.findSystemFonts(fontpaths=None, fontext="ttf")
-    print(
-        f"Found {len(font_paths)} system TrueType fonts in {time() - start:.2f} seconds"
-    )
     # also add OpenType fonts
-    start = time()
     font_paths += font_manager.findSystemFonts(fontpaths=None, fontext="otf")
-    print(
-        f"Found {len(font_paths)} system OpenType fonts in {time() - start:.2f} seconds"
-    )
-    print(f"Found {len(font_paths)} system fonts")
-
     fonts = {}
     for fp in font_paths:
         try:
@@ -58,7 +46,7 @@ def list_system_fonts() -> dict[str:str]:
         except Exception as e:
             # corrupted font? skip
             continue
-    for font, path in sorted(fonts.items()):
+    for font in sorted(fonts.keys()):
         print(f"Font: {font}")
     return fonts
 
@@ -95,36 +83,14 @@ def text(
         )
         # Use the default font if the specified font is not found
         font_path = DEFAULT_FONT_PATH
-    loops = loops_from_text(text, font_path, size, valign)
+    loops = _loops_from_text(text, font_path, size, valign)
     # polys = polygons_with_holes(loops, _is_loop_orientation_reversed(loops))
     polys = _make_polys(loops)
     meshes = [extrude_polygon(poly, height=1.0) for poly in polys]
     return trimesh.util.concatenate(meshes)
 
 
-def text_to_3d_mesh(
-    text: str,
-    font_path: str,
-    pt_size: float = 72,
-    depth: float = 1.0,
-    spacing: int = 0,
-    valign: str = "baseline",
-) -> trimesh.Trimesh:
-    # 1. get all loops (exterior & interior)
-    loops = loops_from_text(text, font_path, pt_size, valign)
-    polys = _make_polys([loop[1] for loop in loops])
-
-    # 2. assemble shapely.Polygon objects with proper holes
-    # polys = polygons_with_holes(loops, _is_loop_orientation_reversed(loops))
-
-    # 3. extrude each polygon (holes stay as voids!)
-    meshes = [extrude_polygon(poly, height=depth) for poly in polys]
-
-    # 4. stitch into one mesh
-    return trimesh.util.concatenate(meshes)
-
-
-def loops_from_text(text, font_path, size=10, valign="baseline"):
+def _loops_from_text(text, font_path, size=10, valign="baseline"):
     # Note: to implement spacing, we need to call ft2font.FT2Font.get_kerning()
     # which requires a pair of glyphs indices, and a KERNING_DEFAULT mode.
     # For now, we will just ignore spacing.
@@ -138,10 +104,6 @@ def loops_from_text(text, font_path, size=10, valign="baseline"):
     loops = []
     for poly in tp.to_polygons():
         verts = np.array(poly)
-        # compute signed area: + → CCW (exterior), – → CW (interior)
-        area2 = _signed_area(verts)
-        kind = "exterior" if area2 > 0 else "interior"
-        # loops.append((kind, verts + np.array([0, y_offset])))
         loops.append(verts + np.array([0, y_offset]))
     return loops
 
@@ -161,61 +123,6 @@ def _calc_y_offset(text: str, fp: FontProperties, valign: str):
     else:
         raise ValueError(f"Invalid valign: {valign}")
     return y_offset
-
-
-def _signed_area(verts):
-    return np.dot(verts[:-1, 0], verts[1:, 1]) - np.dot(verts[:-1, 1], verts[1:, 0])
-
-
-def _is_loop_orientation_reversed(loops):
-    """
-    We assume that the exteriors are CCW and holes (interiors) are CW.
-    We count how many holew are inside exteriors,
-    and how many exteriors are inside exteriors.
-    We should find all holes are inside exteriors,
-    and no exteriors are inside holes.
-    If this is not the case, we will swap exteriors and interiors.
-    """
-    exteriors = [v for k, v in loops if k == "exterior"]
-    interiors = [v for k, v in loops if k == "interior"]
-    holes_within_ext = 0
-    ext_within_holes = 0
-    for ext in exteriors:
-        ext_poly = Polygon(ext)
-        # check if any hole is inside this exterior
-        for hole in interiors:
-            cx, cy = hole[0]  # Check the first point of the hole
-            if ext_poly.contains(Point(cx, cy)):
-                holes_within_ext += 1
-    for hole in interiors:
-        hole_poly = Polygon(hole)
-        # check if any interior is inside this exterior
-        for ext in exteriors:
-            cx, cy = ext[0]
-            if hole_poly.contains(Point(cx, cy)):
-                ext_within_holes += 1
-    return holes_within_ext >= ext_within_holes
-
-
-def polygons_with_holes(loops, loop_orientation_ok):
-    # separate exteriors & interiors
-    exteriors = [v for k, v in loops if k == "exterior"]
-    interiors = [v for k, v in loops if k == "interior"]
-    if not loop_orientation_ok:
-        # swap exteriors and interiors if orientation is not ok
-        exteriors, interiors = interiors, exteriors
-    result = []
-    for ext in exteriors:
-        ext_poly = Polygon(ext)
-        # find holes inside this exterior
-        holes = []
-        for hole in interiors:
-            # sample a point from hole to test containment
-            cx, cy = hole.mean(axis=0)
-            if ext_poly.contains(Point(cx, cy)):
-                holes.append(hole)
-        result.append(Polygon(ext, holes))
-    return result
 
 
 def _make_polys(loops):
