d5f1fe2ab859d8dd626dbbd5c0907b125c93f8b1
Neil Lamoureux <neil.lamoureux@gmail.com>
2025-06-10 20:11:45 -0600

    Fix type hints in lable_atlas

--- src/meshsee/label_atlas.py
+++ src/meshsee/label_atlas.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageFont, ImageDraw
 
 import moderngl
 import numpy as np
-
+from numpy.typing import NDArray
 import os
 
 LABEL_CHARS = "0123456789-."
@@ -13,14 +13,14 @@ FONT_FILE = "DejaVuSansMono.ttf"
 RELATIVE_PATH_TO_FONT = "."
 
 
-def _load_font() -> ImageFont:
+def _load_font() -> ImageFont.FreeTypeFont:
     font_path = os.path.join(
         os.path.dirname(__file__), RELATIVE_PATH_TO_FONT, FONT_FILE
     )
     return ImageFont.truetype(font_path, FONT_SIZE)
 
 
-def _get_font_size(font: ImageFont) -> tuple:
+def _get_font_size(font: ImageFont.FreeTypeFont) -> tuple:
     cell_bbox = font.getbbox("0")
     cell_height = cell_bbox[3]  # Height of a character cell
     cell_width = cell_bbox[2]  # Width of a character cell
@@ -36,16 +36,14 @@ class LabelAtlas:
             data=self._bytes,
             dtype="f1",
         )
-        # self._texture.build_mipmaps()
-        # self._texture.filter = (moderngl.LINEAR_MIPMAP_LINEAR, moderngl.LINEAR)
         self._sampler = ctx.sampler(texture=self._texture)
         self._sampler.filter = (ctx.NEAREST, ctx.NEAREST)
 
-    def uv(self, char: str) -> np.ndarray:
+    def uv(self, char: str) -> NDArray[np.float32]:
         return self._uv_data[char]
 
     @property
-    def image(self) -> Image:
+    def image(self) -> Image.Image:
         return self._image
 
     @property
@@ -56,25 +54,24 @@ class LabelAtlas:
     def sampler(self) -> moderngl.Sampler:
         return self._sampler
 
-    def _create_label_atlas(self) -> tuple[np.ndarray, dict[str, np.ndarray]]:
+    def _create_label_atlas(
+        self,
+    ):
         font = _load_font()
         self._cell_width, self._cell_height = _get_font_size(font)
         self._calc_atlas_size()
         self._draw_chars(LABEL_CHARS, font)
-        self._uv_data = {
-            char: np.array(uv, dtype="f4") for char, uv in self._uv_data.items()
-        }
         self._bytes = self._image.tobytes()
 
     def _save_atlas(self) -> None:
         self._image.save("label_atlas.png")
 
-    def _calc_atlas_size(self) -> tuple[int, int]:
+    def _calc_atlas_size(self):
         cols = len(LABEL_CHARS)
         self._width = cols * self._cell_width
         self._height = self._cell_height
 
-    def _draw_chars(self, chars: str, font: ImageFont) -> None:
+    def _draw_chars(self, chars: str, font: ImageFont.FreeTypeFont) -> None:
         # Create a new image for the atlas
 
         # Create a new image for the atlas
@@ -86,7 +83,9 @@ class LabelAtlas:
         for i, char in enumerate(chars):
             self._draw_char(draw, font, char, i)
 
-    def _draw_char(self, draw: ImageDraw, font, char: str, index: int) -> None:
+    def _draw_char(
+        self, draw: ImageDraw.ImageDraw, font, char: str, index: int
+    ) -> None:
         WHITE = 255
         x = index * self._cell_width
         draw.text((x, 0), char, fill=WHITE, font=font)
@@ -95,11 +94,4 @@ class LabelAtlas:
         v0 = 0.0
         u1 = (x + self._cell_width) / self._width
         v1 = 1.0
-        self._uv_data[char] = (u0, v0, u1, v1)
-
-
-if __name__ == "__main__":
-    ctx = moderngl.create_standalone_context()
-    label_atlas = LabelAtlas(ctx)
-    print(label_atlas.get_uv("0"))
-    print(label_atlas.get_uv("-"))
+        self._uv_data[char] = np.array((u0, v0, u1, v1), dtype="f4")
