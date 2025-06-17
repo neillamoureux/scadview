99a1bdb6100682dbb62036205675c53a807b1d04
Neil Lamoureux <neil.lamoureux@gmail.com>
2025-06-16 22:25:50 -0600

    Ignore ui test in CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-pytest "$@" ${script_dir}/../tests
+echo "script_dir: ${script_dir}"
+current_dir="$(pwd)"
+cd "${script_dir}/.."
+echo "Running tests from $(pwd)"
+pytest "$@"
+cd "${current_dir}"
