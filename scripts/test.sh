0c6f696ad330ef127157cbaf880f81112fcd3110
Neil Lamoureux <neil.lamoureux@gmail.com>
2025-06-16 22:38:07 -0600

    Use pushd / popd if changing dirs in scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,7 @@
 set -euo pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 echo "script_dir: ${script_dir}"
-current_dir="$(pwd)"
-cd "${script_dir}/.."
+pushd "${script_dir}/.."
 echo "Running tests from $(pwd)"
 pytest "$@"
-cd "${current_dir}"
+popd
--- scripts/type.sh
+++ scripts/type.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-current_dir="$(pwd)"
-cd "${script_dir}/.."
+pushd "${script_dir}/.."
 echo "Running type checks from $(pwd)"
 pyright "$@"
-cd "${current_dir}"
+popd
