63aa45514debd1531118890c5e5b67c7def42004
Neil Lamoureux <neil.lamoureux@gmail.com>
2025-06-17 21:58:11 -0600

    Remove unused imports

--- src/meshsee/controller.py
+++ src/meshsee/controller.py
@@ -7,7 +7,6 @@ from trimesh import Trimesh
 from trimesh.exchange import export
 
 from meshsee.module_loader import ModuleLoader
-from meshsee.observable import Observable
 
 logger = logging.getLogger(__name__)
 
