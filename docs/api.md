aaf1b560743c60fe472aca40d0dd6b670f196f44
Neil Lamoureux <neil.lamoureux@gmail.com>
2025-07-21 21:43:10 -0600

    Adjust headings for api, and add more api functions at meshsee level

--- docs/api.md
+++ docs/api.md
@@ -3,6 +3,8 @@
 ::: meshsee.Color
 ::: meshsee.set_mesh_color
 ::: meshsee.surface
+::: meshsee.mesh_from_heightmap
 ::: meshsee.text
+::: meshsee.manifold_to_trimesh
 
 
--- src/meshsee/__init__.py
+++ src/meshsee/__init__.py
@@ -1,6 +1,6 @@
-from meshsee.api.colors import (
-    Color as Color,
-    set_mesh_color as set_mesh_color,
-)
+from meshsee.api.colors import Color as Color
+from meshsee.api.colors import set_mesh_color as set_mesh_color
+from meshsee.api.surface import mesh_from_heightmap as mesh_from_heightmap
 from meshsee.api.surface import surface as surface
 from meshsee.api.text_builder import text as text
+from meshsee.api.utils import manifold_to_trimesh as manifold_to_trimesh
