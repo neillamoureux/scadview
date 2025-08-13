b1714fc7237732eb26f3fa07a2bf83d80afc2ca9
Neil Lamoureux <neil.lamoureux@gmail.com>
2025-08-12 20:59:28 -0600

    Add AI-genearated linear extrude with tests

--- src/meshsee/__init__.py
+++ src/meshsee/__init__.py
@@ -1,5 +1,6 @@
 from meshsee.api.colors import Color as Color
 from meshsee.api.colors import set_mesh_color as set_mesh_color
+from meshsee.api.linear_extrude import linear_extrude
 from meshsee.api.surface import mesh_from_heightmap as mesh_from_heightmap
 from meshsee.api.surface import surface as surface
 from meshsee.api.text_builder import text as text
