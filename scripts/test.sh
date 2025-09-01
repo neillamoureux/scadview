3f1e5871680534972516d3488d2d3d62a67dfefb
Neil Lamoureux <neil.lamoureux@gmail.com>
2025-09-01 15:04:28 -0600

    Add debug stmts for CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -euox pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 echo "script_dir: ${script_dir}"
 pushd "${script_dir}/.."
 echo "Running tests from $(pwd)"
+echo "CI is ${CI:-}"
 
 # In CI, prevent external plugin autoload (blocks pytest-qt import of Qt),
 # and explicitly disable pytest-qt just in case.
 if [[ -n "${CI:-}" ]]; then
-#   export PYTEST_DISABLE_PLUGIN_AUTOLOAD=1
+  export PYTEST_DISABLE_PLUGIN_AUTOLOAD=1
   extra="-p no:pytestqt"
 else
   extra=""
