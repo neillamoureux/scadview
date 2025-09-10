c2890d021b6ea7d049e6cc86b994a31596475358
neillamoureux <neil.lamoureux@gmail.com>
2025-09-09 20:53:13 -0600

    Add debug flag; enable using in make (#35)
    Also remove redundant exception / log when loading the mesh script file fails.

--- src/meshsee/app.py
+++ src/meshsee/app.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 
 from meshsee.controller import Controller
@@ -12,9 +13,41 @@ logger = logging.getLogger(__name__)
 
 
 def main():
+    set_logging_level()
     logger.info("Meshee app starting up")
     renderer_factory = RendererFactory(CameraPerspective())
     gl_widget_adapter = GlWidgetAdapter(renderer_factory)
     controller = Controller()
     GlUi(controller, gl_widget_adapter).run()
     logger.info("Meshee app stopping")
+
+
+def set_logging_level():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Increase verbosity (-v=INFO, -vv=DEBUG)",
+    )
+    parser.add_argument(
+        "--log-level",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="Set the logging level directly",
+    )
+
+    args = parser.parse_args()
+
+    if args.log_level:
+        level = getattr(logging, args.log_level)
+    elif args.verbose == 1:
+        level = logging.INFO
+    elif args.verbose >= 2:
+        level = logging.DEBUG
+    else:
+        level = logging.WARNING
+
+    logger = logging.getLogger()
+    logger.warning(f"Setting debug to {level} ({logging.getLevelName(level)})")
+    logger.setLevel(level=level)
