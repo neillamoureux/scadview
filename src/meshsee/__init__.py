587211cc89df65ce996232684c8b5dac6bdb11b6
Neil Lamoureux <neil.lamoureux@gmail.com>
2025-09-22 20:21:14 -0600

    Add text_polys to just create polygons

--- src/meshsee/__init__.py
+++ src/meshsee/__init__.py
@@ -4,4 +4,5 @@ from meshsee.api.linear_extrude import linear_extrude as linear_extrude
 from meshsee.api.surface import mesh_from_heightmap as mesh_from_heightmap
 from meshsee.api.surface import surface as surface
 from meshsee.api.text_builder import text as text
+from meshsee.api.text_builder import text_polys as text_polys
 from meshsee.api.utils import manifold_to_trimesh as manifold_to_trimesh
