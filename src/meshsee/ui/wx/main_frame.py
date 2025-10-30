5bc9233c43e56809adfef19a6c39a7985a88955e
Neil Lamoureux <neil.lamoureux@gmail.com>
2025-10-30 15:14:38 -0600

    Place Action classes into own file

--- src/meshsee/ui/wx/main_frame.py
+++ src/meshsee/ui/wx/main_frame.py
@@ -1,151 +1,17 @@
 import logging
 
 import wx
-from typing import Any, Callable, TypeVar, Generic
-
 
 from meshsee.controller import Controller
 from meshsee.mesh_loader_process import LoadResult
 from meshsee.render.gl_widget_adapter import GlWidgetAdapter
+from meshsee.ui.wx.action import Action, CheckableAction, ChoiceAction
 from meshsee.ui.wx.moderngl_widget import create_graphics_widget
-from meshsee.observable import Observable
 
 logger = logging.getLogger(__name__)
 
 LOAD_CHECK_INTERVAL_MS = 10
 INITIAL_FRAME_SIZE = (900, 600)
-
-
-class Action:
-    def __init__(
-        self,
-        label: str,
-        callback: Callable[[wx.Event], None],
-        accelerator: str | None = None,
-    ):
-        self._label = label
-        self._callback = callback
-        self._accelerator = accelerator
-        self._id: int = wx.NewIdRef()
-
-    def button(self, parent: wx.Window) -> wx.Button:
-        btn = wx.Button(parent, label=self._label, id=self._id)
-        btn.Bind(wx.EVT_BUTTON, self._callback)
-        return btn
-
-    def menu_item(self, menu: wx.Menu) -> wx.MenuItem:
-        label = self._menu_item_label()
-        item = menu.Append(self._id, label)
-        menu.Bind(wx.EVT_MENU, self._callback, item)
-        return item
-
-    def _menu_item_label(self):
-        return (
-            f"{self._label}\tCtrl+{self._accelerator}"
-            if self._accelerator
-            else self._label
-        )
-
-
-class CheckableAction(Action):
-    def __init__(
-        self,
-        label: str,
-        callback: Callable[[wx.Event], None],
-        initial_state: bool,
-        on_state_change: Observable,
-        accelerator: str | None = None,
-    ):
-        super().__init__(label, callback, accelerator)
-        on_state_change.subscribe(self._on_state_change)
-        self._initial_state = initial_state
-        self._menu_items: list[wx.MenuItem] = []
-        self._checkboxes: list[wx.CheckBox] = []
-
-    def menu_item(self, menu: wx.Menu):
-        label = self._menu_item_label()
-        item = menu.AppendCheckItem(self._id, label)
-        item.Check(self._initial_state)
-        menu.Bind(wx.EVT_MENU, self._update_state, item)
-        self._menu_items.append(item)
-        return item
-
-    def checkbox(self, parent: wx.Window) -> wx.CheckBox:
-        chk = wx.CheckBox(parent, label=self._label, id=self._id)
-        chk.Bind(wx.EVT_CHECKBOX, self._update_state)
-        chk.SetValue(self._initial_state)
-        self._checkboxes.append(chk)
-        return chk
-
-    def _update_state(self, event: wx.Event):
-        self._callback(event)
-
-    def _on_state_change(self, state: bool):
-        for item in self._menu_items:
-            item.Check(state)
-        for chk in self._checkboxes:
-            chk.SetValue(state)
-
-
-class ChoiceAction:
-    def __init__(
-        self,
-        labels: list[str],
-        values: list[str],
-        callback: Callable[[wx.CommandEvent, str], None],
-        initial_value: str,
-        on_value_change: Observable,
-    ):
-        on_value_change.subscribe(self._on_value_change)
-        self._labels = labels
-        self._values = values
-        self._callback = callback
-        self._initial_value = initial_value
-        self._id: int = wx.NewIdRef()
-        self._radio_buttons: list[wx.RadioButton] = []
-        self._menu_items: list[wx.MenuItem] = []
-
-    def _on_value_change(self, value: str):
-        for rb, v in zip(self._radio_buttons, self._values):
-            rb.SetValue(v == value)
-        for item, v in zip(self._menu_items, self._values):
-            item.Check(v == value)
-
-    def menu_items(self, menu: wx.Menu) -> list[wx.MenuItem]:
-        for label, value in zip(self._labels, self._values):
-            item = menu.AppendRadioItem(id=wx.ID_ANY, item=label)
-            item.Check(self._initial_value == value)
-            menu.Bind(wx.EVT_MENU, self._update_value_from_menu, item)
-            self._menu_items.append(item)
-        return self._menu_items
-
-    def radio_buttons(self, parent: wx.Window) -> list[wx.RadioButton]:
-        self._radio_buttons: list[wx.RadioButton] = []
-        first = True
-        for label, value in zip(self._labels, self._values):
-            if first:
-                rb = wx.RadioButton(parent, label=label, style=wx.RB_GROUP)
-            else:
-                rb = wx.RadioButton(parent, label=label)
-            rb.SetValue(value == self._initial_value)
-            rb.Bind(
-                wx.EVT_RADIOBUTTON,
-                self._update_value_from_radio_button,
-            )
-            self._radio_buttons.append(rb)
-        return self._radio_buttons
-
-    def _update_value_from_radio_button(self, e: wx.CommandEvent):
-        for label, value in zip(self._labels, self._values):
-            if label == e.GetEventObject().GetLabel():
-                self._callback(e, value)
-
-    def _update_value_from_menu(self, e: wx.CommandEvent):
-        event_item_id = e.GetId()
-        for item, value in zip(self._menu_items, self._values):
-            item_id = item.GetId()
-            if e.IsChecked() and event_item_id == item_id:
-                self._callback(e, value)
 
 
 class MainFrame(wx.Frame):
