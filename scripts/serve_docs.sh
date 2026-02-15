f186d653bccb55df553eb6db00c0539a88e3acc2
Neil Lamoureux <neil.lamoureux@gmail.com>
2026-02-14 20:38:39 -0700

    chore: Enable versioned docs using mike

--- scripts/serve_docs.sh
+++ scripts/serve_docs.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 pushd "${script_dir}/.." >/dev/null
-# mkdocs serve --config-file "${script_dir}/../mkdocs.yml" "$@"
-mkdocs serve 
+mkdocs serve "$@"
+
 popd >/dev/null
